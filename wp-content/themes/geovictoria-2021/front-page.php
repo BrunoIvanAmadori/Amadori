1d6aedb574e07dd7eb3cea3ffd8a755ef8fc21a6
unknown <brunoivanamadori@gmail.com>
2021-08-24 15:57:09 -0300

    adding images, further stylizing, fonts fix

--- wp-content/themes/geovictoria-2021/front-page.php
+++ wp-content/themes/geovictoria-2021/front-page.php
@@ -19,24 +19,25 @@ get_header();
 		<div class="container d-flex justify-content-between h-100">
 			<div class="col-6 d-flex">
 				<div class="align-self-center pe-3">
-					<h1 class="gray mb-3">
+					<h1 class="gray mb-3 fw-bold">
 						Optimiza la gestión humana ahorrando tiempo, recursos y dinero
 					</h1>
 					<h3 class="gray fw-light mb-3">
 						Mas de 10 años innovando para nuestros clientes.
 					</h3>
-					<button class="button__bigblue">
-						<span class="white">Solicita tu demo gratis </span>
+					<button class="button--bigblue">
+						<span class="white">Solicita tu demo gratis</span>
 					</button>
 				</div>
 			</div>
-			<div class="col-6 bg-primary h-100%">
+			<div class="col-6 h-100">
+				<img src='<?php echo get_template_directory_uri(); ?>/img/monitor.png' class='img-fluid'>
 			</div>
 		</div>
 	</section>
 
 	<section class='container trusting-companies'>
-		<h2 class="text-center gray mb-3">Empresas que confian en nosotros</h2>
+		<h2 class="text-center gray mb-3 fw-bold">Empresas que confian en nosotros</h2>
 		<div class="d-flex justify-content-between">
 			<div>thyssenkrup</div>
 			<div>securitas</div>
@@ -49,18 +50,51 @@ get_header();
 		</div>
 	</section>
 
+	<div class="bg-head-gray"></div>
+
+
 	<section class='container products'>
-		<h2 class="text-center gray mb-3 fw-light">Te acompañamos durante toda<br> <b>tu experiencia Geovictoria</b></h2>
-		<div class="d-flex justify-content-between">
-			<div>Portal asistencia</div>
-			<div>Portal acceso</div>
-			<div>Portal comedor</div>
+		<h2 class="text-center gray mb-3"><span class="fw-light">Te acompañamos durante toda</span><br> <b>tu experiencia Geovictoria</b></h2>
+		<div class="row d-flex justify-content-center">
+			<div class="col d-flex justify-content-between">
+				<div class='product--box d-flex flex-column align-content-center'>
+					<img class="product__icon" src="<?php echo get_template_directory_uri()?>/img/product-asistencia.svg"/>
+					<h4>Portal <br>asistencia</h4>	
+					<a href="#">Ver más</a>
+				</div>
+				<div class='product--box d-flex flex-column align-content-center'>
+					<img class="product__icon" src="<?php echo get_template_directory_uri()?>/img/product-acceso.svg"/>
+					<h4>Portal <br>acceso</h4>
+					<a href="#">Ver más</a>
+				</div>
+				<div class='product--box d-flex flex-column align-content-center'>
+					<img class="product__icon" src="<?php echo get_template_directory_uri()?>/img/product-comedor.svg"/>
+					<h4>Portal <br>comedor</h4>
+					<a href="#">Ver más</a>
+				</div>
+			</div>
 		</div>
-		<div class="w-100% text-center">Business Inteligence</div>
+		<div class="row d-flex justify-content-center">
+			<div class="col d-flex justify-content-between">
+				<div class="product--card d-flex flex-column align-items-center">
+					<div class='row'>
+						<div class="col justify-content-center text-end">
+							<img class="product__icon" src="<?php echo get_template_directory_uri()?>/img/product-business.svg"/>
+						</div>
+						<div class="col d-flex flex-column justify-content-center text-start">
+							<h4>Business Intelligence</h4>
+							<a href="#">Ver más</a>
+						</div>
+					</div>
+					
+
+				</div>
+			</div>
+		</div>
 	</section>
 
 	<section class='container'>
-	<h2 class="text-center gray mb-3 fw-light">Te acompañamos durante toda<br> <b>tu experiencia Geovictoria</b></h2>
+	<h2 class="text-center gray mb-3"><span class="fw-light">Te acompañamos durante toda</span><br> tu experiencia Geovictoria</h2>
 		<div class="d-flex justify-content-between">
 			<div>Contacto</div>
 			<div>Kickoff</div>
@@ -69,25 +103,26 @@ get_header();
 			<div>Soporte</div>
 		</div>
 	</section>
-
-	<section class="cta container-fluid">
+	<div class="bg-head-blue container-fluid"></div>
+	<section class="hero container-fluid bg-blue-2">
+		
 		<div class="container d-flex justify-content-between">
-			<div class="col-6">
-				<h1>
+			<div class="align-self-center pe-3">
+				<h1 class="white">
 					Soporte y atención personalizada
 				</h1>
-				<h2>
-				Siempre contarás con un equipo humano que te apoyará en cada momento
-				</h2>
-				<p>
-				GeoVictoria cuenta con una mesa de soporte multicanal a través de chat en linea, telefono, correo electrónico, que estará disponible 24/7 para resolver todas tus dudas guiarte en el uso de nuestra plataforma
+				<h3 class="fw-light white">
+					Siempre contarás con un equipo humano que te apoyará en cada momento
+				</h3>
+				<p class="fw-light white">
+					GeoVictoria cuenta con una mesa de soporte multicanal a través de chat en linea, telefono, correo electrónico, que estará disponible 24/7 para resolver todas tus dudas guiarte en el uso de nuestra plataforma
 				</p>
-				<button class="button__bigblue">
-				Contáctanos ahora
+				<button class="fw-bold button--bigwhite">
+					Contáctanos ahora
 				</button>
 			</div>
-			<div class="col-6 bg-primary h-100%">
-
+			<div class="col-6 h-100 d-flex justify-content-center">
+				<img class="hero__img" src="<?php echo get_template_directory_uri()?>/img/soporte-personalizado.png"/>
 			</div>
 		</div>
 	</section>
