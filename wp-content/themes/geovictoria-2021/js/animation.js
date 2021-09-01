371f36883b364e5e95264efbe609c5d2eb24e76d
unknown <brunoivanamadori@gmail.com>
2021-08-31 21:15:06 -0300

    Refactored and commented the animation loading feature

--- wp-content/themes/geovictoria-2021/js/animation.js
+++ wp-content/themes/geovictoria-2021/js/animation.js
@@ -1,9 +1,9 @@
+// I play whatever is visible in the viewport
+
 var observer = new IntersectionObserver(
   (entries) => {
-    console.log(entries);
     entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        
+      if (entry.isIntersecting) {  
         playTarget(entry.target);
     }
   });
@@ -11,65 +11,63 @@ var observer = new IntersectionObserver(
 
 let $triggers = [];
 
-function addAnimationTrigger( $triggerEl, $animation) {
-  observer.observe($triggerEl);
-
-    
-    
-  let lazyImages = [];
+function addAnimationTrigger( element, animation ) {
+  observer.observe(element);
+  lazyLoad(element, animation);
+}
   
-  $triggerEl.childNodes.forEach( el => {
+function lazyLoad(element, animation) {
+  let elementImages = [];
+  
+  // the element contains all the images of the animation
+  element.childNodes.forEach( el => {
     if (el.nodeName == 'IMG') {
-      console.log('el');
-      lazyImages.push(el);
+      elementImages.push(el);
     }
   });
 
-  if (lazyImages.length == 0 ) {
-    $triggers.push({trigger: $triggerEl, hasPlayed: 0, animation: $animation, finishedLoading: true  });
+  if (elementImages.length == 0 ) {
+    // if there are not images on the element, then take it as already loaded
+    $triggers.push({trigger: element, hasPlayed: 0, animation: animation, finishedLoading: true  });
   } else {
-    loadingCount = 0;
+    // if we got images, then let's load them
+    imagesLoaded = 0;
 
-    lazyImages.forEach( img => {
-      
-
+    elementImages.forEach( img => {
+      // if images are already preloaded by cache...
       if (img.complete && img.naturalHeight !== 0) {
-        // console.log(img);
-        loadingCount++;
-        console.log('cargo');
-        console.log(loadingCount);
-        // console.log(lazyImages.length);
-
-        if (loadingCount == lazyImages.length ) {
-          console.log('submiteo');
-          let $finishedLoading = true;
-         
-          $triggers.push({trigger: $triggerEl, hasPlayed: 0, animation: $animation, finishedLoading: $finishedLoading  });  
-        }
+        // add image loaded
+        imagesLoaded++;
       }
-      
-      img.addEventListener("load", () => {
-        console.log('added');
-        loadingCount++;
-        if (loadingCount == lazyImages.length ) {
-          let $finishedLoading = true;
-         
-          $triggers.push({trigger: $triggerEl, hasPlayed: 0, animation: $animation, finishedLoading: $finishedLoading  });
-          $triggers.forEach( ($trig) => {
-            
-            if ($trig.finishedLoading == true && isInViewport($trig.trigger) == true && $trig.hasPlayed == 0 ) {
-              $trig.animation.play();
-              $trig.hasPlayed = 1;
-            }
-          })
-  
-        }
-      });
-    });
-    }
-  }
-  
+      // check if all the images were preloaded
+      if (imagesLoaded == elementImages.length ) {
 
+        // Add a new animation trigger as finished
+        $triggers.push({ trigger: element, hasPlayed: 0, animation: animation, finishedLoading: true  });  
+
+      } else {
+
+        // As there are images left, I'll check when they finish loading
+        img.addEventListener("load", () => {
+
+          // When image is loaded, add to the count
+          imagesLoaded++;
+          
+          // re-check if all the images are loaded
+          if (imagesLoaded == elementImages.length ) {       
+            $triggers.push({ trigger: element, hasPlayed: 0, animation: animation, finishedLoading: true });
+            
+            // The observer doesn't fires the animation if target is already on viewport,
+            // so I fire it here...
+            if (isInViewport($triggers[$triggers.length - 1].trigger)) {
+              playTarget($triggers[$triggers.length - 1].trigger);
+            }
+          };
+        });
+      }
+    });
+  }
+}
 
 function isInViewport(myElement) {
   var bounding = myElement.getBoundingClientRect();
@@ -77,48 +75,20 @@ function isInViewport(myElement) {
   var myElementWidth = myElement.offsetWidth;
 
   if (bounding.top >= -myElementHeight 
-      && bounding.left >= -myElementWidth
-      && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + myElementWidth
-      && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
-      return true;
+    && bounding.left >= -myElementWidth
+    && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + myElementWidth
+    && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
+    return true;
   } else {
-      return false;
+    return false;
   }
 }
-let a = 0;
+
 function playTarget(el) {
   $triggers.forEach( $trig => {
-
-   if (el == $trig.trigger && $trig.hasPlayed == 0) {
-    console.log($trig.trigger);
-    console.log('has played');
-     $trig.animation.play();
-     $trig.hasPlayed = 1;
-    
-  //   let promise = new Promise ( (res, rej) => {
-  //         if ($trig.finishedLoading == true) {
-  //           $trig.animation.play();
-  //           res();
-  //         }
-  //  });
-  //  promise.then( function () { $trig.animation.play(); $trig.hasPlayed = 1; });
-  }
-});
+    if ( el == $trig.trigger && $trig.finishedLoading == true && $trig.hasPlayed == 0 ) {
+      $trig.animation.play();
+      $trig.hasPlayed = 1;
+    }
+  });
 }
-   //console.log($trig.class.substr(1));
-    // if (el.classList.contains($trig.class.substr(1)) && $trig.hasPlayed === 0 && $trig.finishedLoading == true) {
-    //   $trig.animation.play();
-    //   console.log($trig);
-    //   $trig.hasPlayed = 1;
-    //  // console.log($trig);
-    // } else {
-    //   // let promise = new Promise ( (res, rej) => {
-    //   //   if ($trig.finishedLoading == true) {
-    //   //     res('..');
-    //   //   }
-    //   // });
-
-      
-    // }
-
-   
