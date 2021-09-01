752d274704f506f491d721081f6e762f35f5a5ec
unknown <brunoivanamadori@gmail.com>
2021-09-01 08:09:12 -0300

    mend

--- wp-content/themes/geovictoria-2021/js/sticky.js
+++ wp-content/themes/geovictoria-2021/js/sticky.js
@@ -5,100 +5,6 @@
 
 let lastScrollTop = 0;
 
-// Get the header
-let header = document.getElementById("masthead");
-let headerHeight = header.offsetHeight;
-
-// get the topheader
-
-let topheader = document.getElementById("tophead");
-let topheaderHeight = topheader.offsetHeight;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-
-let scrollingDown;
-let scrollingUp;
-let viewport;
-let size;
-
-let throttled = false;
-
-function setHeaderPosition () {
-
-  if (isPageOnTop() && viewport == "md") {
-    header.style.top = "0px";
-    size = 0;
-  } else {
-    header.style.top = "0px";
-    size = 0;
-  }
-
-  if (!isPageOnTop() && viewport != "md") {
-    header.style.top = "0px";
-    topheader.style.top = "-40px"
-    size = 80;
-  }
-
-  if (isPageOnTop() && viewport != "md") {
-    header.style.top = "40px";
-    topheader.style.top = "0px";
-    size = 120;
-    show(value = "-40", 0 );
-    console.log(size);
-  }
-}
-
-function getViewport() {
-  if(  window.innerWidth  < 990 ) {
-    viewport = 'md';
-  } else {
-    viewport = 'lg';
-  }
-}
-
-function isPageOnTop() {
-  if (window.pageYOffset == 0) {
-    return true;
-  } else {
-    return false;
-  }
- }
-
-function show(s, e) {
-  anime({
-    duration: 150,
-    targets: ['#tophead', '#masthead'],
-    translateY: [s, e],
-    easing: 'easeInOutSine'
-    });
-}
-
-function hide(s, e) {
-  anime({
-    duration: 150,
-    targets: ['#tophead', '#masthead'],
-    translateY: [s, e],
-    easing: 'easeInOutSine'
-    });
-}
-
-function onScrollDown() {
-  if(scrollingDown != true){
-    hide (0, -size);
-  }
-  scrollingDown = true;
-  scrollingUp = false;
-
-}
-
-function onScrollUp() {
-  if(scrollingUp != true){
-    show(-size, 0);
-  }
-  scrollingUp = true;
-  scrollingDown = false;
-}
-
 window.onload = () => {
   window.addEventListener('resize', () => {
     if (!throttled) {
@@ -109,9 +15,11 @@ window.onload = () => {
   
       setTimeout( function() {
         throttled = false
-      }, 100);
+      }, 20);
     }
-  });
+    // start timing for event "completion"
+    } 
+  );
 
   window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
     let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
@@ -125,7 +33,220 @@ window.onload = () => {
     }
     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
   }, false);
+  
 }
+
+
+
+// stickyheader class should be part of this plugin
+
+// Get the header
+let header = document.getElementById("masthead");
+let headerHeight = header.offsetHeight;
+
+// get the topheader
+
+let topheader = document.getElementById("tophead");
+let topheaderHeight = topheader.offsetHeight;
+
+// Get the offset position of the navbar
+let sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
+let isOnTop = 0;
+let scrollingDown = 0;
+let scrollingUp = 0;
+let isFullMenu;
+let viewport;
+let size;
+
+let throttled = false;
+let translated = false;
+function setHeaderPosition () {
+  console.log('ocurrio header position');
+
+
+  if (viewport == "lg" && !translated) {
+    //    header.style.top = "0px";
+    if (isPageOnTop()) {
+      topheader.style.transform= 'translateY(0px)';
+      header.style.transform= 'translateY(0px)';
+      actualPosition = 0;
+    } else {
+      if (scrollingDown == true) {
+        topheader.style.transform= 'translateY(-120px)';
+        header.style.transform= 'translateY(-120px)';
+        actualPosition = -120;
+      } else {
+        topheader.style.transform= 'translateY(-40px)';
+        header.style.transform= 'translateY(-40px)';
+        actualPosition = -40;
+      }
+    }
+    
+    translated = true;
+     //actualPosition= -120;
+      }
+    
+
+  if (isPageOnTop() && viewport == "md" && isFullMenu) {
+  //  topheader.style.transform= 'translateY(0px)';
+  //  header.style.transform= 'translateY(0px)';
+//   header.style.top = "40px";
+//   topheader.style.top = "0px";
+    
+    // size= 120;
+  }
+
+  if (isPageOnTop() && viewport == "lg") {
+    // console.log('isfullmenu desk')
+    // header.style.transform= 'translateY(40px)';
+//    header.style.top = "40px";
+    // size= 80;
+    isFullMenu = true;
+  }
+
+  if (viewport == "md" && translated) {
+//    header.style.top = "0px";
+    header.style.transform= 'translateY(-40px)';
+    translated = false;
+ //    actualPosition= -120;
+  }
+
+  
+
+  if (!isPageOnTop() && viewport == "lg") {
+ //   header.style.transform= 'translateY(-40px)';
+ //   topheader.style.transform= 'translateY(-40px)';
+//    header.style.top = "0px";
+//    topheader.style.top = "-40px"
+    // size = 80;
+  }
+
+  if (isPageOnTop() && viewport == "lg") {
+ //   topheader.style.transform= 'translateY(0px)';
+ //   header.style.transform= 'translateY(40px)';
+ //   header.style.top = "40px";
+ //   topheader.style.top = "0px";
+  
+    // size = 120;
+   // show(value = "-40", 0 );
+   // console.log(size);
+  }
+}
+
+function getViewport() {
+  if(  window.innerWidth  < 992 ) {
+    viewport = 'md';
+    // 
+  } else {
+    viewport = 'lg';
+  }
+}
+
+function isPageOnTop() {
+  if (window.pageYOffset == 0) {
+    return true;
+  } else {
+    return false;
+  }
+ }
+
+const tl = anime.timeline({   });
+let actualPosition = 0;
+function show(s, e) {
+  anime({
+    duration: 300,
+    targets: ['#tophead', '#masthead'],
+    translateY: [s, e],
+    easing: 'easeInOutSine'
+    });
+    actualPosition = e;
+    console.log(actualPosition);
+}
+
+// function hide(s, e) {
+//   anime({
+//     duration: 150,
+//     targets: ['#tophead', '#masthead'],
+//     translateY: [s, e],
+//     easing: 'easeInOutSine'
+//     });
+
+// }
+
+function onScrollDown() {
+  if(scrollingDown != true){
+//    console.log(size);
+    if (viewport == "lg" ) {
+      if (isFullMenu) {
+        show (actualPosition, actualPosition - 120);
+      }
+      if (!isFullMenu) {
+        show (actualPosition, actualPosition - 80);
+      }
+    };
+
+    if (viewport == "md" ) {
+      actualPosition = -40;
+      show (actualPosition, actualPosition - 80);
+    };
+
+    // if (viewport == "lg" && !isFullMenu ) {
+    //   show (0, -80);
+    // };
+
+
+    // if (viewport == "md") {
+    //   show (0, -80);
+    // };
+    
+    
+  }
+  scrollingDown = true;
+  scrollingUp = false;
+  isFullMenu = false;
+
+}
+
+function onScrollUp() {
+  let st = window.pageYOffset;
+  
+
+  if(scrollingUp == false){
+    if (viewport == "md" ) {
+      actualPosition = -120;
+      show (actualPosition, actualPosition + 80);
+    };
+
+    if (viewport == "lg" ) {
+      show (actualPosition, actualPosition + 80);
+    };
+    scrollingUp = true;
+  }
+
+  if(st < 100 && scrollingUp === true && !isFullMenu){
+    if (viewport == "lg" ) {
+      show (actualPosition, actualPosition + 40);
+      isFullMenu = true;
+    };
+    // scrollingUp = false;
+  }
+
+  
+  scrollingUp = true;
+  scrollingDown = false;
+  
+}
+
+
 
 getViewport();
 setHeaderPosition();
+
+if (viewport == "md") {
+  header.style.transform= 'translateY(-40px)';
+} else {
+  header.style.transform= 'translateY(0px)';
+}
